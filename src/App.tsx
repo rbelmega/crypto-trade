5f0ae792a313ad51aa7f6a2e7c519e6544ec770e
Rostyslav Belmeha <belmega31@gmail.com>
2022-01-05 23:01:49 +0200

    feat: application development

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material";
+
+import DataGrid from "./components/Table";
 import "./App.css";
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={darkTheme}>
+        <div className="App-header">
+          <DataGrid />
+        </div>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
